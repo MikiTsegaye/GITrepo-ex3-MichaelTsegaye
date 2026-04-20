5386633507e7cec914295773aa22679d74181832
MikiTsegaye <milki2014@gmail.com>
2026-04-20 11:32:16 +0300

    Update files

--- MichaelTsegaye.java
+++ MichaelTsegaye.java
@@ -1,7 +1,7 @@
 public class MichaelTsegaye {
 	public static void main(String[] args) {
 		for (int i = 0; i < 5; i++) {
-			System.out.println("MichaelTsegaye");
+			System.out.println("MichaelTsegaye is learning DevOps!");
 		}
 	}
 }
